922fe1351e9b741507a863ad118bcd72c00994e7
joybo <joybo@BOSS>
2020-08-13 11:56:49 +0530

    Name in Hindi

---
+++ Homework/src/Name.java
@@ -0,0 +1,25 @@
+
+public class Name {
+
+	public static void main(String[] args) {
+
+		char ch1='\u091C';
+		char ch1a = '\u094C';
+		char ch2 = '\u092F';
+		char ch3 = '\u092E';
+		char ch4 = '\u093E';
+		char ch5 = '\u0932';
+		char ch6 = '\u092F';
+		
+		char ch7 = '\u092C';
+		char ch8 = '\u093F';
+		char ch9 = '\u0938';
+		char ch10= '\u0935';
+		char ch11= '\u093E';
+		char ch12= '\u0938';
+		
+		System.out.println("ch is: "+ch1+ch1a+ch2+ch3+ch4+ch5+ch6+" "+ch7+ch8+ch9+ch10+ch11+ch12);	
+
+	}
+
+}
